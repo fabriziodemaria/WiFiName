7d03adfe8c85f011a1ed8f88b7c127a52d8bb4e4
Fabrizio Demaria <fabrizio.f.demaria@gmail.com>
2015-01-08 21:43:09 +0100

    Added menu to quit; added signal strength

--- StatusBarApp/AppDelegate.m
+++ StatusBarApp/AppDelegate.m
@@ -34,9 +34,27 @@
     
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleInterfaceNotification:) name:CWSSIDDidChangeNotification object:nil];
     [[NSUserNotificationCenter defaultUserNotificationCenter] setDelegate:self];
+    // create menu
+    NSMenu *menu = [[NSMenu alloc] initWithTitle:@""];
+    NSMenuItem *item2 = [[NSMenuItem alloc] initWithTitle:@"Quit" action:@selector(menu2Action) keyEquivalent:@""];
+    [menu addItem:item2];
+    
+    [_statusItem setMenu:menu]; // attach
+    //[self performSelectorInBackground:@selector(stren) withObject:nil];
+}
+
+-(void)menu2Action{
+    [[NSApplication sharedApplication] terminate:self];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
+}
+
+-(void)stren{
+    while (true) {
+        [NSThread sleepForTimeInterval:1];
+        NSLog(@"%ld", self.wif.rssiValue);
+    }
 }
 
 
@@ -59,6 +77,7 @@
     }
     [_statusItem setTitle:shortString];
     [self showNotification:self.wif.ssid];
+    
 }
 
 
@@ -68,17 +87,53 @@
 }
 
 - (IBAction)showNotification:(NSString *)wifiname{
+    
     NSUserNotification *notification = [[NSUserNotification alloc] init];
-    if(wifiname!=nil){
-    notification.title = @"New WiFi connection";
-    NSString *namemsg = [NSString stringWithFormat:@"WiFi name: %@",
-                         wifiname];
-    notification.informativeText = namemsg;
-    } else {
+    if(wifiname==nil){
         notification.title = @"WiFi disconnected";
     }
     //notification.soundName = NSUserNotificationDefaultSoundName;
     [[NSUserNotificationCenter defaultUserNotificationCenter] deliverNotification:notification];
+
+    if(wifiname!=nil){
+    
+    long sum = 0;
+    for(int i = 0; i < 1; i++) {
+        long tmp[1];
+        
+        [NSThread sleepForTimeInterval:1];
+        NSLog(@"%ld", self.wif.rssiValue);
+        tmp[i] = self.wif.rssiValue;
+        sum = sum + tmp[i];
+    }
+    
+    long tot = sum/1;
+    NSString *power = nil;
+    
+    if(tot>-40){
+        power = [NSString stringWithFormat:@"exceptional signal"];
+    }
+    if(tot<=-40 && tot >-55){
+        power = [NSString stringWithFormat:@"very good signal"];
+    }
+    if(tot<=-55 && tot >-70){
+        power = [NSString stringWithFormat:@"good signal"];
+    }
+    if(tot<=-70 && tot >-80){
+        power = [NSString stringWithFormat:@"weak signal"];
+    }
+    if(tot<=-80){
+        power = [NSString stringWithFormat:@"very weak signal"];
+    }
+    
+    NSUserNotification *notification = [[NSUserNotification alloc] init];
+    notification.title = @"New WiFi connection";
+    NSString *namemsg = [NSString stringWithFormat:@"WiFi name: %@, %@",
+                         wifiname, power];
+    notification.informativeText = namemsg;
+    //notification.soundName = NSUserNotificationDefaultSoundName;
+    [[NSUserNotificationCenter defaultUserNotificationCenter] deliverNotification:notification];
+    }
 }
 
 - (void)itemClicked:(id)sender {
